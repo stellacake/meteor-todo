a8a02acf52aaa55775f8535cb50b4bb4327f3911
Stella <stella.dmt@gmail.com>
2021-02-24 11:13:29 +0100

    added security

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -23,24 +23,26 @@ export const App = () => {
 
 	const pendingOnlyFilter = { ...hideCompletedFilter, ...userFilter };
 
-	const tasks = useTracker(() => {
-		if (!user) {
-			return [];
+	const { tasks, pendingTasksCount, isLoading } = useTracker(() => {
+		const noDataAvailable = { tasks: [], pendingTasksCount: 0 };
+		if (!Meteor.user()) {
+			return noDataAvailable;
 		}
-		return TasksCollection.find(
+		const handler = Meteor.subscribe("tasks");
+
+		if (!handler.ready()) {
+			return { ...noDataAvailable, isLoading: true };
+		}
+
+		const tasks = TasksCollection.find(
 			hideCompleted ? pendingOnlyFilter : userFilter,
 			{
-				sort: { createdAt: 1 },
+				sort: { createdAt: -1 },
 			}
 		).fetch();
-	});
+		const pendingTasksCount = TasksCollection.find(pendingOnlyFilter).count();
 
-	const pendingTasksCount = useTracker(() => {
-		if (!user) {
-			return 0;
-		}
-
-		return TasksCollection.find(pendingOnlyFilter).count();
+		return { tasks, pendingTasksCount };
 	});
 
 	const pendingTasksTitle = `${
@@ -71,6 +73,9 @@ export const App = () => {
 								{hideCompleted ? "Show All" : "Hide Completed"}
 							</button>
 						</div>
+
+						{isLoading && <div className="loading">loading...</div>}
+
 						<ul className="tasks">
 							{tasks.map((task) => (
 								<Task
